973cb7c699bdf3718b5046395b2dff9f836311ae
LAPTOP-EHDI9A5M\sarat <sarath@armino.in>
2022-01-26 19:32:53 +0530

    first commit

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav>
+    <v-toolbar>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <span class="font-weight-light">Sample</span>
+        <span> Project</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer
+     
+      app
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-layout justify-end>
+        <v-icon @click="close" class="black--text">mdi-close</v-icon>
+      </v-layout>
+      <v-list dense rounded>
+        <v-list-item v-for="item in items" :key="item.title" link :to="item.route">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    drawer: false,
+    items:[
+      {title :"Home",icon:"mdi-home",route:"/"} ,  
+      {title :"About",icon:"mdi-account",route:"/about"} , 
+      {title :"Gallery",icon:"mdi-image",route:"/gallery"} , 
+    ]
+  }),
+  methods: {
+    close() {
+      this.drawer = false;
+    },
+  },
+};
+</script>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#fbe8d4',
+                secondary: '#991a1a',
+                accent: '#8c9eff',
+                error: '#b71c1c',
+            },
+        },
+    },
+});
