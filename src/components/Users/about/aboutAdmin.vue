788a29d13392e882e0edb87f64c1a57404e91eb8
Sarath-Mar <sarathbnm@gmail.com>
2023-03-07 09:56:53 +0530

    test

--- src/components/Users/about/aboutAdmin.vue
+++ src/components/Users/about/aboutAdmin.vue
@@ -25,7 +25,7 @@
               class="px-2 text-justify"
               :class="$vuetify.breakpoint.xs ? 'mt-5' : 'mt-2'"
             >
-              <span class="stories"> {{ aboutData.postText }}</span>
+              <span class="stories"> !{{ aboutData.postText }}</span>
               <!-- <span class="stories"> {{ items.content }}</span> -->
             </v-flex>
           </v-layout>
@@ -53,7 +53,7 @@ export default {
       color: "#1F7087",
       src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
       content:
-        "Photography is the art, application, and practice of creating durable images by recording light, either electronically by means of an image sensor, or chemically by means of a light-sensitive material such as photographic film. It is employed in many fields of science, manufacturing (e.g., photolithography), and business, as well as its more direct uses for art, film and video production, recreational purposes, hobby, and mass communication. Typically, a lens is used to focus the light reflected or emitted from objects into a real image on the light-sensitive surface inside a camera during a timed exposure. With an electronic image sensor, this produces an electrical charge at each pixel, which is electronically processed and stored in a digital image file for subsequent display or processing. The result with photographic emulsion is an invisible latent image, which is later chemically developed into a visible image, either negative or positive, depending on the purpose of the photographic material and the method of processing. A negative image on film is traditionally used to photographically create a positive image on a paper base, known as a print, either by using an enlarger or by contact printing.",
+        "! Photography is the art, application, and practice of creating durable images by recording light, either electronically by means of an image sensor, or chemically by means of a light-sensitive material such as photographic film. It is employed in many fields of science, manufacturing (e.g., photolithography), and business, as well as its more direct uses for art, film and video production, recreational purposes, hobby, and mass communication. Typically, a lens is used to focus the light reflected or emitted from objects into a real image on the light-sensitive surface inside a camera during a timed exposure. With an electronic image sensor, this produces an electrical charge at each pixel, which is electronically processed and stored in a digital image file for subsequent display or processing. The result with photographic emulsion is an invisible latent image, which is later chemically developed into a visible image, either negative or positive, depending on the purpose of the photographic material and the method of processing. A negative image on film is traditionally used to photographically create a positive image on a paper base, known as a print, either by using an enlarger or by contact printing.",
       artist: "Madhuraj",
     },
     aboutData: {},
